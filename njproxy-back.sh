038556b819a967e90d2e995b33f9cc583729a614
U201413497 <33078674+U201413497@users.noreply.github.com>
2026-05-04 18:30:59 +0800

    Update njproxy-back.sh

--- njproxy-back.sh
+++ njproxy-back.sh
@@ -1,1 +1,24 @@
-1
+#!/bin/bash
+
+_INSTALL(){
+mkdir /root/njproxy-back && cd /root/njproxy-back
+wget https://github.com/U201413497/njproxy/releases/download/njproxy-back/backend && chmod +x backend
+touch /etc/systemd/system/backend.service
+echo "
+[Unit]
+Description=Backend Service
+After=network-online.target
+Wants=network-online.target systemd-networkd-wait-online.service
+
+[Service]
+ExecStart=/root/njproxy-back/backend
+Restart=always
+RestartPreventExitStatus=23
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/backend.service
+systemctl enable backend
+systemctl restart backend
+}
+
+_INSTALL
