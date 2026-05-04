d2bbb2bdda8a2f809e3003dc0b08d0b7c6d407a0
U201413497 <33078674+U201413497@users.noreply.github.com>
2026-05-04 18:34:40 +0800

    Update njproxy-front.sh

--- njproxy-front.sh
+++ njproxy-front.sh
@@ -1,1 +1,24 @@
-1
+#!/bin/bash
+
+_INSTALL(){
+mkdir /root/njproxy-front && cd /root/njproxy-front
+wget https://github.com/U201413497/njproxy/releases/download/njproxy-front/frontend && chmod +x frontend
+touch /etc/systemd/system/frontend.service
+echo "
+[Unit]
+Description=Frontend Service
+After=network-online.target
+Wants=network-online.target systemd-networkd-wait-online.service
+
+[Service]
+ExecStart=/root/njproxy-front/frontend
+Restart=always
+RestartPreventExitStatus=23
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/frontend.service
+systemctl enable frontend
+systemctl restart frontend
+}
+
+_INSTALL
